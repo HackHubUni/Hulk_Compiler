f854f63243640749e8b102564048f05f6a39b39b
Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>
2024-03-28 14:17:00 -0400

    Serialize Parser

--- Hulk/Parser/Hulk_Parser.py
+++ Hulk/Parser/Hulk_Parser.py
@@ -1,8 +1,30 @@
-from Lexer_Parser.shift_reduce import LR1Parser, evaluate_reverse_parse
+import os
+import dill
+from Lexer_Parser.shift_reduce import LR1Parser
 from Hulk.Grammar.gramarlr1 import Gramarlr1
 
-Grammar = Gramarlr1().Grammar
+# Definir la ruta del archivo serializado
+save_dir = 'save'
+os.makedirs(save_dir, exist_ok=True)  # Crea la carpeta si no existe
+file_path = os.path.join(save_dir, 'parser.pkl')
 
-parser = LR1Parser(Grammar)
-
-print("Ok")
+# Verificar si el archivo serializado existe
+if os.path.exists(file_path):
+    try:
+        # Intentar cargar el objeto serializado
+        with open(file_path, 'rb') as f:
+            parser = dill.load(f)
+    except EOFError:
+        # Si hay un error EOF, eliminar el archivo corrupto y crear un nuevo objeto
+        os.remove(file_path)
+        Grammar = Gramarlr1().Grammar
+        parser = LR1Parser(Grammar)
+        with open(file_path, 'wb') as f:
+            dill.dump(parser, f)
+else:
+    # Si no existe, crear el objeto y serializarlo
+    Grammar = Gramarlr1().Grammar
+    parser = LR1Parser(Grammar)
+    with open(file_path, 'wb') as f:
+        dill.dump(parser, f)
+print("Ready Parser")
