b4cc1783ec3ec853db0e66c216deb6a813785caf
Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>
2024-03-30 21:14:00 -0400

    Reset from grammar,

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Hulk.Lexer.Hulk_Lexer import get_hulk_lexer
 from Hulk.Parser.Hulk_Parser import get_hulk_parser
 from Hulk.AST_Semantic.Ast import *
-from Hulk.AST_Semantic.type_checker import Collector,TypeBuilder
+
 from cmp.evaluation import evaluate_reverse_parse
 
 parser = get_hulk_parser()
@@ -57,15 +57,3 @@ print(len(tokens),"len tokens")
 
 
 ast = evaluate_reverse_parse(parsedd, operationsdd, tokens)
-errors = []
-collector = Collector(errors)
-collector.visit(ast)
-print(f"El ast es \n {ast}")
-context = collector.context
-print(context)
-builder = TypeBuilder(context, errors)
-builder.visit(ast)
-
-print('Errors:', errors)
-print('Context:')
-print(context)
