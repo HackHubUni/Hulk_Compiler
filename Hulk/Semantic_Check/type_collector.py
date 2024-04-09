98e0a4d1c321e65fdd809cc4516e46f269986e6b
Leismael Sosa <leismael.sosa17@gmail.com>
2024-04-09 01:59:48 -0400

    Re-implementation of the Type Collector

--- Hulk/Semantic_Check/type_collector.py
+++ Hulk/Semantic_Check/type_collector.py
@@ -72,102 +72,81 @@ class TypeCollector:
         self.current_protocol: ProtocolInfo = None
 
     @visitor.on("node")
-    def visit(self, node: AstNode, scope: HulkScopeLinkedNode):
+    def visit(self, node: AstNode, scope: HulkScopeLinkedNode, from_where: str = ""):
         pass
 
     @visitor.when(ProgramNode)
-    def visit(self, node: ProgramNode, scope: HulkScopeLinkedNode):
+    def visit(
+        self, node: ProgramNode, scope: HulkScopeLinkedNode, from_where: str = ""
+    ):
         for declaration in node.declarations:
-            self.visit(declaration, self.global_scope)
+            self.visit(declaration, self.global_scope, "In the program")
 
     @visitor.when(TypeDeclarationNode)
-    def visit(self, node: TypeDeclarationNode, scope: HulkScopeLinkedNode):
+    def visit(
+        self, node: TypeDeclarationNode, scope: HulkScopeLinkedNode, from_where: str
+    ):
+        from_where += f" in the declaration of the type with name '{node.type_name}',"
         if node.type_name in builtin_types:
             self.errors.append(
                 SemanticError(
-                    f"Type {node.type_name} is a builtin type and cannot be redefined"
+                    f"{from_where} the Type '{node.type_name}' is a builtin type and cannot be redefined"
                 )
             )
             node.type_name = get_unique_name_with_guid(node.type_name)
-        elif scope.is_type_defined(node.type_name):
+        elif scope.is_type_or_protocol_defined(node.type_name):
             self.errors.append(
-                SemanticError(f"Type {node.type_name} is already defined")
+                SemanticError(
+                    f"{from_where} the name '{node.type_name}' is already defined in another Type or Protocol"
+                )
             )
             node.type_name = get_unique_name_with_guid(node.type_name)
         fix_parent_type_id(node)
         scope.define_type(node.type_name)
 
     @visitor.when(ProtocolDeclarationNode)
-    def visit(self, node: ProtocolDeclarationNode, scope: HulkScopeLinkedNode):
+    def visit(
+        self, node: ProtocolDeclarationNode, scope: HulkScopeLinkedNode, from_where: str
+    ):
+        from_where += (
+            f" in the declaration of the protocol with name '{node.protocol_name}',"
+        )
         if node.protocol_name in builtin_protocols:
             self.errors.append(
                 SemanticError(
-                    f"Protocol {node.protocol_name} is a builtin protocol and cannot be redefined"
+                    f"{from_where} the Protocol '{node.protocol_name}' is a builtin protocol and cannot be redefined"
                 )
             )
             node.protocol_name = get_unique_name_with_guid(node.protocol_name)
-        elif scope.is_protocol_defined(node.protocol_name):
+        elif scope.is_type_or_protocol_defined(node.protocol_name):
             self.errors.append(
-                SemanticError(f"Protocol {node.protocol_name} is already defined")
+                SemanticError(
+                    f"{from_where} the name '{node.protocol_name}' is already defined in another type or protocol"
+                )
             )
             node.protocol_name = get_unique_name_with_guid(node.protocol_name)
 
-        # Check if the parent of the protocol is not the same protocol. Because that's not possible
-        # TODO: Remove this and do it in the next pass
-        if node.parent and node.parent == node.protocol_name:
-            self.errors.append(
-                SemanticError(f"Protocol '{node.protocol_name}' cannot extends itself")
-            )
-            node.parent = None
-
-        protocol_info: ProtocolInfo = scope.define_protocol(node.protocol_name)
-        self.current_protocol = protocol_info
-        # Now we define the protocol methods
-        for method in node.methods:
-            self.visit(method, scope)
-
-    # TODO: Remove this and added it in the next pass
-    @visitor.when(ProtocolMethodNode)
-    def visit(self, node: ProtocolMethodNode, scope: HulkScopeLinkedNode):
-        method_name: str = node.protocol_method_name
-        method_return_type: str = node.return_type
-
-        if not scope.is_type_defined(method_return_type):
-            self.errors.append(
-                SemanticError(
-                    f"Return type {method_return_type} is not defined in the scope"
-                )
-            )
-            method_return_type = ErrorType.static_name()
-        # Now the arguments
-        arguments: list[VariableInfo] = [
-            get_variable_info_from_var_def(arg) for arg in node.arguments
-        ]
-        if self.current_protocol.is_method_defined(method_name):
-            self.errors.append(
-                SemanticError(
-                    f"Method {method_name} is already defined in protocol {self.current_protocol.name}"
-                )
-            )
-            node.protocol_method_name = get_unique_name_with_guid(method_name)
-            method_name = node.protocol_method_name
-        method_info_base: MethodInfoBase = MethodInfoBase(
-            method_name, arguments, method_return_type
-        )
-        self.current_protocol.define_method(method_info_base)
+        scope.define_protocol(node.protocol_name)
 
     @visitor.when(FunctionDeclarationNode)
-    def visit(self, node: FunctionDeclarationNode, scope: HulkScopeLinkedNode):
+    def visit(
+        self, node: FunctionDeclarationNode, scope: HulkScopeLinkedNode, from_where: str
+    ):
+        from_where += (
+            f" in the declaration of the function with name '{node.function_name}'"
+        )
         if node.function_name in builtin_functions:
             self.errors.append(
                 SemanticError(
-                    f"Function {node.function_name} is a builtin function and cannot be redefined"
+                    f"{from_where} the name '{node.function_name} 'is a builtin function and cannot be redefined"
                 )
             )
             node.function_name = get_unique_name_with_guid(node.function_name)
-        elif scope.function_defined(node.function_name):
+        elif scope.is_function_defined(node.function_name):
             self.errors.append(
-                SemanticError(f"Function {node.function_name} is already defined")
+                SemanticError(
+                    f"{from_where} the name '{node.function_name}' is already defined"
+                )
             )
             node.function_name = get_unique_name_with_guid(node.function_name)
 
